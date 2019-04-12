cb461b4d7f69b330f17269c437c799a1cd547239
Dennis Warner <dennis.warner@gmail.com>
2019-04-11 21:27:01 -0400

    First Successful comparison between DIAmonds and our artificial Security

--- 0_Prep.R
+++ 0_Prep.R
@@ -8,4 +8,3 @@ v.tickers<-df.DJDict$Symbol[1:30];
 fnin<-paste(dirDocs, "/dt.Common.csv", sep = "");
 dt.Common<-read_csv(fnin)
 dt.CommonIndSec <-  dt.Common %>% dplyr::select(.,ticker, sector, industry);
-df.DJIAComposition<-read.xlsx(paste(dirDocs,"/DJIAComposition.xlsx",sep=""),sheetName="DJIAComposition");
--- 1_DJIMain.R
+++ 1_DJIMain.R
@@ -3,102 +3,100 @@ dirProject          <-  paste(dirOneDrive,"/DelesinDJI/DDJI",sep="");
 source(paste(dirProject, "/0_AllFunctions.R", sep = ""));
 source(paste(dirProject, "/0_LoadLibs.R", sep = ""));
 dirDocs             <-   paste(dirProject, "/Docs", sep = "");
-
 source(paste(dirProject,"/0_Prep.R",sep=""));
 dirData        <- "D:/WarkleighD/WarkleighData";
-dirPriceData   <- paste(dirData,"/AdjOpens/Sectors",sep="");
 
-#original data from MH
+BUILD<-FALSE;
+if(BUILD){
+    df.X.xts       <- f.buildDJIdata()
+}else{
+    fnin<-"dt.X.xts.RDATA";
+    ss_dt.X.xts<-load(fnin);   #silently load dt.X.xts modelling data
+}
 
-fnin         <- "DJ_OrigData.csv";
-df.data      <- read_csv(fnin);
-df.data.xts  <- f.dfall2xts(df.data);  # Get mike's original data in xts form
-firstdate    <- index(first(df.data.xts));
-lastdate     <- index(last(df.data.xts));
-origspan     <- paste(firstdate,"/",lastdate,sep="");
-df.odata.xts<-df.data.xts%>%f.xts2df()%>%dplyr::select(Date,DIA,X.INDU,Divisor)%>%f.df2xts();
-#load the original weights
+
+#load the original weights--------------------
 fnin<-"Docs/DJIWeights.csv";
 df.weights<-read.csv(fnin)
-v.changedates<- df.DJIAComposition$ChangeDates;
-nchangedates <-length(v.changedates);
-firstgood<-v.changedates[nchangedates-1];
-lastgood <-v.changedates[nchangedates]-1;
-goodspan<-paste(firstgood,"/",lastgood,sep="");
-
-#Load all the component data
-v.tickers<-names(df.DJIAComposition)[-1]
-
-v.v<-v.tickers[v.tickers%in% c("DOW","AAPL"]
-dt.CO<-dt.Common%>%dplyr::filter(ticker %in% v.tickers)%>%filter(ticker!="DOW")%>%filter(ticker!="AAPL"); #select the dictionary entries for the 30 DJI stocks
-#load aapl to get date range
-
-fnin<-paste(dirPriceData,"/Technology/AAPL.RDATA",sep="");
-ss_dt.p<-load(fnin)
-dt.p<-dt.p%>%dplyr::select(date,closeunadj)
-dt.Prices.xts<-f.df2xtsD(dt.p,"date")
-names(dt.Prices.xts)[1]<-"AAPL";
-v.tickers<-dt.CO$ticker;
-iticker<-0;
-while(iticker<length(v.tickers)){
-    iticker<-iticker+1;
-    symb<-v.tickers[iticker];
-    #if(!is.na(match(symb,dt.CO$ticker))){
-        dt.coo<-dt.CO%>%filter(ticker==symbol)
-        #cat(iticker,symb,dt.coo$ticker,"\n");
-        fnin<-paste(dirPriceData,"/",dt.coo$sector,"/",dt.coo$ticker,".RDATA",sep="");
-        ss_dt.p<-load(fnin)
-        dt.p<-dt.p%>%dplyr::select(date,closeunadj)
-        dt.p.xts<-f.df2xtsD(dt.p,"date")
-        names(dt.p.xts)[1]<-symb;
-        dt.Prices.xts<-merge.xts(dt.Prices.xts,dt.p.xts,join='left');
-    #}
-    #cat(iticker,symbol,match(ticker,dt.CO$ticker),"\n");
-}
-dt.X.xts<-merge.xts(dt.Prices.xts,df.odata.xts,join='left')[origspan];
-fnout<-"dt.X.xts.RDATA";
-save(dt.X.xts,file=fnout);
-dt.X<-dt.X.xts%>%f.xts2df()%>%dplyr::select(AAPL, BA, CAT, HD, INTC,JNJ,KO,MCD,MMM,MSFT,PFE,PG,UNH,WMT,DIA,X.INDU,Divisor)
+#----must convert prices to log-relative ror 
+dt.X<-dt.X.xts%>%f.xts2df()%>%dplyr::select(AAPL, BA, CAT, HD, INTC,JNJ,KO,MCD,MMM,MSFT,PFE,PG,UNH,WMT,DIA,INDU,Divisor)
 m.X<-as.matrix(dt.X[,1:14]);
+
 m.R<-
 mvweights<-df.weights[,1]
 YMV<-m.X%*%df.weights$MV
-#
-#----All data is held in dt.X.xts
-#step through each data span in the  titak data set
+#simulate owning the DJIA or owning the stocks with weights.
+#to avoid confusion  start with $1000000, and fractional investment
+m.shares<-matrix(0,30,2)
+#start Date
+# option 1 invest in
+dim(dt.X)   #the data set has 2515 days
+v.DIA<-dt.X$DIA
 
 
-iticker<-0;
-#Assemble the price data from ShaRadar----
-while(iticker<nrow(dt.CO)){
-    iticker<-iticker+1;
-    ticker<-dt.CO$ticker[iticker]
-    sectic<-paste(dt.CO$sector[iticker],"/",ticker,".RDATA",sep="");
-    fnin<-paste(dirPriceData,"/",sectic,sep="");
-    
-    ss_dt.p<-load(fnin); #silently load the dt.p structure for this ticker
-    dt.pp<-dt.p%>%dplyr::select(date,closeunadj)
-    dt.pp.xts<-f.df2xtsD(dt.pp,"date")[span]
-    v.x<-coredata(dt.pp.xts$closeunadj)
-   
-    if(iticker==1){
-        m.djp<-matrix(0,length(v.x),30)
-        m.djp[,1]<-v.x
-    }else{
-        m.djp[,iticker]<-v.x
-    }
+v.proportion<-0.01*df.weights$MV;
+v.money<-v.proportion*1000000;
+v.shares<-0*v.money;
+for(i in c(1:14)){
+    cat(i,"\n");
+    v.shares[i]<-f.safeDiv(v.money[i],m.X[1,i])
 }
-colnames(m.djp)<-v.tickers
-df.djp.xts<-xts(m.djp,order.by=index(dt.pp.xts))
-df.djp<-df.djp.xts%>%f.xts2df()
-#----
-#compute
-#----get the supplementary data from original data set----
-df.partial.xts    <- df.data.xts[,c("DIA","$INDU","Computed","Divisor")]
-#add it to the ShaRadar component data
-df.X.xts          <- merge.xts(df.djp.xts,df.partial.xts, join='left');   #merge, based on date
-df.X              <- f.xts2df(df.X.xts)     #convert to a regular data.frame
-#Begin computation
-df.X$Sum30<-rowSums(df.X[,2:31])
-df.X<-df.X%>%dplyr::mutate(DJIAhat=Sum30/Divisor,Error=X.INDU-DJIAhat)
-summary(df.X)
+m.X[1,]%*%v.shares
+v.delwealth<-m.X%*%v.shares
+
+nDIA<-1000000/v.DIA[1]
+v.diawealth<-nDIA*v.DIA
+v.shares
+plot.ts(dt.X$DIA)
+
+v.shares[is.na(v.shares)]<-0;
+
+df.investments<-data.frame(v.diawealth,v.delwealth)%>%dplyr::mutate(dif=v.delwealth-v.diawealth,rDIA=f.roc(v.diawealth),rDEL=f.roc(v.delwealth),rdif=rDEL-rDIA)
+
+# 
+# 
+# v.wealth<-m.X%*%v.shares
+# 
+# v.minvest<-df.weights$MV*
+# 
+# #
+# #----All data is held in dt.X.xts
+# #step through each data span in the  titak data set
+# f.ror<-function(x){
+#     r<-x/lag(x)
+# }
+# 
+# iticker<-0;
+# #Assemble the price data from ShaRadar----
+# while(iticker<nrow(dt.CO)){
+#     iticker<-iticker+1;
+#     ticker<-dt.CO$ticker[iticker]
+#     sectic<-paste(dt.CO$sector[iticker],"/",ticker,".RDATA",sep="");
+#     fnin<-paste(dirPriceData,"/",sectic,sep="");
+#     
+#     ss_dt.p<-load(fnin); #silently load the dt.p structure for this ticker
+#     dt.pp<-dt.p%>%dplyr::select(date,closeunadj)
+#     dt.pp.xts<-f.df2xtsD(dt.pp,"date")[span]
+#     v.x<-coredata(dt.pp.xts$closeunadj)
+#    
+#     if(iticker==1){
+#         m.djp<-matrix(0,length(v.x),30)
+#         m.djp[,1]<-v.x
+#     }else{
+#         m.djp[,iticker]<-v.x
+#     }
+# }
+# colnames(m.djp)<-v.tickers
+# df.djp.xts<-xts(m.djp,order.by=index(dt.pp.xts))
+# df.djp<-df.djp.xts%>%f.xts2df()
+# #----
+# #compute
+# #----get the supplementary data from original data set----
+# df.partial.xts    <- df.data.xts[,c("DIA","$INDU","Computed","Divisor")]
+# #add it to the ShaRadar component data
+# df.X.xts          <- merge.xts(df.djp.xts,df.partial.xts, join='left');   #merge, based on date
+# df.X              <- f.xts2df(df.X.xts)     #convert to a regular data.frame
+# #Begin computation
+# df.X$Sum30<-rowSums(df.X[,2:31])
+# df.X<-df.X%>%dplyr::mutate(DJIAhat=Sum30/Divisor,Error=X.INDU-DJIAhat)
+# summary(df.X)
