39464e0911e48e36350fb772108087d91b85cf7f
Dennis Warner <dennis.warner@gmail.com>
2019-05-31 03:04:53 -0400

    Begin Backtest programming

--- 0_fPortfolioFunctions.R
+++ 0_fPortfolioFunctions.R
@@ -20,8 +20,43 @@ setMethod("turnpoints", "timeSeries",
                          documentation = x@documentation)
           }
 )
-f.fPortfolioEstimates<-function(df.x.xts,df.dictentry){
-    #optimize portfolio using fPortfolio package
+f.fPortFullSim<-function(df.x.xts,df.dictentry){
+    freq<-"months";
+    trainyears<-2;
+    testyears<-1;
+    l.PortData   <-   f.prepPortData(df.x.xts,freq,trainyears,testyears);
+    names(l.PortData)
+    # begin the era loop
+    
+    df.Eras<-l.PortData$df.Eras;
+    df.x.xts<-l.PortData$df.x.xts
+    df.R.xts<-l.PortData$df.R.xts
+    df.R.ts<-as.timeSeries(df.R.xts);
+    v.dates<-l.PortData$v.dates
+    iera<-0;
+    while(iera<l.PortData$numEras){
+        iera<-iera+1;
+        ftrain<-df.Eras$fTrno[iera];
+        ltrain<-df.Eras$lTrno[iera];
+        ftest<-df.Eras$fTsto[iera];
+        ltest<-df.Eras$lTsto[iera];
+        Rtrain<-df.R.ts[ftrain:ltrain,];
+        Rtest<-df.R.ts[ftest:ltest,];
+        #the data have been extracted now derive the optimum weights
+
+        cat(iera,as.Date(v.dates[df.Eras$fTrno[iera]]),
+            df.Eras$lTrno[iera],
+            df.Eras$fTsto[iera],
+            df.Eras$lTsto[iera],
+            df.Eras$TstSpan[iera],"\n")
+    }
+    
+    
+    
+    
+    
+}
+f.fPoertfolioEstimates2<-function(df.x.xts,df.dictentry){
     #convert xts to time series as used in fPortfolio package
     df.x.xts<-na.locf(df.x.xts,na.rm=FALSE,fromLast = TRUE)
     df.xx.ts<-as.timeSeries(df.x.xts)
@@ -31,228 +66,208 @@ f.fPortfolioEstimates<-function(df.x.xts,df.dictentry){
     df.rr.ts[1,]<-0;
     #and to cumulative returns
     df.cr.ts<-cumulated(df.rr.ts)
-    # drawdowns----
-    dd<-drawdowns(df.r.ts);
-    #drawdownstats
-    EQIX<-df.cr.ts[,"EQIX"];
-    dd<-drawdowns(EQIX)
-    #ds<-drawdownsStats(dd);
-    EQIX.LW<-lowess(EQIX,f=0.03);
-    plot(EQIX)
-    lines(EQIX.LW,col="brown",lwd=2);
-    #locate turning points-------
-    suppressMessages(library(pastecs));
-    EQIX.TP<-turnpoints(EQIX.LW)
-    EQIX.PEAKS<-EQIX.TP[EQIX.TP@recordIDs[,"peaks"]==TRUE,]
-    EQIX.PITS <- EQIX.TP[EQIX.TP@recordIDs[, "pits"] == TRUE, ]
-    plot(EQIX);
-    lines(EQIX.LW,col="brown",lwd=2);
-    points(EQIX.PEAKS, col = "green3", pch = 24)
-    #----------------------------------------------
-    #Basoc Statistics of Financial Assets----
+    etfname<-str_sub(df.dictentry$ETF,1,-3)
+    #Basic Statistics of Financial Assets----
     df.stats<-basicStats(df.r.ts[, 1:6])
     colMeans(100 * df.r.ts,na.rm=TRUE);
     df.ra.ts<-df.r.ts;df.ra.ts[is.na(df.ra.ts)]<-0;
-    Covariance <- round(cov(100 * df.ra.ts), digits = 4)
-    skewness(EQIX)
-    kurtosis(EQIX)
-    #compute quantiles for all columns jointly, using method recommended for CVAR
-    quantile(df.r.ts, probs = seq(0, 1, 0.25), type = 1,na.rm=TRUE)
-    #a particular quantile for each column separately
-    colQuantiles(df.r.ts, prob = 0.05, type = 1)
-    # 3 risk measures in fPortfolio
-    # first sample covariance risk of a simple, equally weighted 3 component portfolio
-    covRisk(100*df.ra.ts[,1:3], weights = c(1, 1, 1)/3)
-    # for value at risk
-    varRisk(100*df.ra.ts[,1:3], weights = c(1, 1, 1)/3, alpha = 0.05)
-    
-    # for conditional value at risk
-   
-    cvarRisk(100*df.ra.ts[,1:3], weights = c(1, 1, 1)/3, alpha = 0.05)
-    CVaR.5%
-    #can apply anty function across the columns, e.g.
-    
-    round(colStats(df.ra.ts, FUN = "median"),
-          digits = 4)
-    
-    # there are a number of function that can be applied to the cumulants of columns
-    #
-    #--------------------------------------------------------------------------------
+    Covariance <- round(cov(100 * df.ra.ts), digits = 4);
     #  Plotting
-    plot(df.ra.ts[,1:10], main = "XLU components", col = "steelblue")
+    stitle<-paste(etfname," Components")
+    plot(df.ra.ts[,1:10], main = stitle, col = "steelblue")
     
     myset<-df.cr.ts[,1:5]
     plot(myset, plot.type = "single", , col = 1:5, xlab = "Date",
-           ylab = "XLU components")
+         ylab = "stitle")
     ttl<-paste(names(df.cr.ts)[1:5],sep="+")
     title(main = ttl)
     hgrid()
-   plot(myset[,2],myset[,5]) #scatter plots
-    #There are 3 preconfigured plots
-   
-   boxPlot(returns(myset[,1:4]))
-   boxPercentilePlot(returns(myset[,1:4]))
-   
-   histPlot(df.ra.ts[,2])
-   densityPlot(df.ra.ts[,2]);
-   logDensityPlot(df.ra.ts[,2])
-   
-   qqnormPlot(df.ra.ts[,2])
-   
-  
-   qqnigPlot(df.ra.ts[,2])
-   qqghtPlot(df.ra.ts[,2])
-   
-   #Customizing Plots
-   seriesPlot(df.cr.ts[,2])
-   SBAC<-df.cr.ts[,2];
-   seriesPlot(SBAC, title = FALSE)
-   title(main = "SBACt", xlab = "", ylab = "Index")
-   text(as.POSIXct("2006-11-25"), rev(SBAC)[1], as.character(rev(SBAC)[1]),
-          font = 2)
-   mtext("Source: SWX", side = 4, col = "grey", adj = 0, cex = 0.7); #"Margin text strings"
-   #Decorating the plot
-  seriesPlot(SBAC, title = FALSE)
-  title(main = "SBAC Performance Index", xlab = "", ylab = "SBAC Index")
-  text(as.POSIXct("2006-11-25"), rev(SBAC)[1], as.character(rev(SBAC)[1]),
-          font = 2)
-   mtext("Source: SBC", side = 4, col = "grey", adj = 0, cex = 0.7)
-   #more decorations
-   seriesPlot(SBAC, grid = FALSE, box = FALSE, rug = FALSE)
-   hgrid()
-   boxL()
-   copyright()
-   abline(h = 350, col = "orange")
-   #------------------------------------------------------------------------------------------
-   #   Modelling Asset Returns
-   #------------------------------------------------------------------------------------------
-   # Test whether returns arenormal
-   Sr<-df.rr.ts[,2]
-   shapiroTest <- assetsTest(df.rr.ts[, 1:3], method = "shapiro")
-  print(shapiroTest)
-     #slotNames(shapiroTest)
-     names(shapiroTest)
-   
-   #7.2 FITTING ASSET RETURNS
-     # first try the student's T distribuion
-     fit <- assetsFit(df.rr.ts[, 1:3], method = "st")
-     print(fit)
-   #Can also use the copula estimation approach, use pckge fCopula
-    #---------------------------------------------------------------------------------------------
-    #
-    # Selecting similar or dissimilar assets
-     #
-     # 
+    plot(myset[,2],myset[,4]) #scatter plots
     
-   
-     XLUData<-df.rr.ts
-     hclustComplete <- assetsSelect(XLUData, method = "hclust")
+    myData<-df.rr.ts;
+    v.etf.ts<-myData[,ncol(myData)]
+    myStocks<-myData[,-ncol(myData)];
+    v.stocks<-names(myStocks);
+    nStocks<-ncol(myStocks);
+    hclustComplete <- assetsSelect(myStocks, method = "hclust")
     hclustComplete
-    plot(hclustComplete, xlab="XLU compnent assets")
-     Call:
-         hclust(d = dist(t(x), method = measure), method = method)
-     #Cluster method : complete
-     #Distance : euclidean
-     #Number of objects: 9
-  plot(hclustComplete, xlab = "XLU Components")
-    mtext("Distance Metric: Euclidean", side = 3) 
-     
-     
-     
-    hclustWard <- assetsSelect(XLUData, method = "hclust", control = c(measure = "euclidean",
-                                                                         method = "ward.D"))
-    hclustWard
-   plot(hclustWard)
-  mtext("Distance Metric: Euclidean", side = 3)
-  #---------------------------Grouping using eigenvalues
+    
+    plot(hclustComplete, xlab=stitle)
+    #---------grouping using kmeans analysis
+    kmeans <- assetsSelect(myStocks, method = "kmeans", control <- c(centers = 10,
+                                                                   algorithm = "Hartigan-Wong"))
+    sort(kmeans$cluster)
+    library(fMultivar)
+    v.stocks   <- assetsArrange(myStocks, method = "hclust"); #arrange by hierarchical cluistering
+    myStocks2<-100*myStocks[,v.stocks];
+    myData2<-100*myData;
+    library(fPortfolio);
+    
+    #First dow equally weighted portfolio
+    ewSpec<-portfolioSpec();  #create default (MV) portfoliospec
+    nAssets<-ncol(myData2)-1
+    setWeights(ewSpec) <- rep(1/nAssets, times = nAssets)
+    ewPortfolio <- feasiblePortfolio(
+        data = myStocks2,
+        spec = ewSpec,
+        constraints = "LongOnly")
+    print(ewPortfolio)
+    col <- divPalette(ncol(myStocks2), "RdBu")
+    weightsPie(ewPortfolio, radius = 0.7, col = col)
+    mtext(text = "Equally Weighted MV Portfolio", side = 3, line = 1.5,
+          font = 2, cex = 0.7, adj = 0)
+    weightedReturnsPie(ewPortfolio, radius = 0.7, col = col)
+    covRiskBudgetsPie(ewPortfolio, radius = 0.7, col = col)
+    #----------------------------------------------------------
+    # Minimum risk portfolio given a target return
+    #----------------------------------------------------------
+    minriskSpec <- portfolioSpec();
+    targetReturn <- getTargetReturn(myPortfolio@portfolio)["mean"]
+    setTargetReturn(minriskSpec) <- targetReturn
+    minriskPortfolio <- efficientPortfolio(
+        data = myStocks2,
+        spec = minriskSpec,
+        constraints = "LongOnly")
+    print(minriskPortfolio)
+    weightsPie(minriskPortfolio, radius = 0.7, col = col)
+    mtext(text = "Min Risk MV Portfolio", side = 3, line = 1.5,
+          font = 2, cex = 0.7, adj = 0)
+    weightedReturnsPie(minriskPortfolio, radius = 0.7, col = col)
+    covRiskBudgetsPie(minriskPortfolio, radius = 0.7, col = col)
+    
+    
+    col <- qualiPalette(ncol(myStocks2), "Dark2");
+                        
+    weightsPie(minriskPortfolio, radius = 0.7, col = col)
+    mtext(text = "Minimal Risk MV Portfolio", side = 3, line = 1.5,
+            font = 2, cex = 0.7, adj = 0)
+    
+    weightedReturnsPie(minriskPortfolio, radius = 0.7, col = col)
+    mtext(text = "Minimal Risk MV Portfolio", side = 3, line = 1.5,
+            font = 2, cex = 0.7, adj = 0)
+    covRiskBudgetsPie(minriskPortfolio, radius = 0.7, col = col)
+    mtext(text = "Minimal Risk MV Portfolio", side = 3, line = 1.5,
+            font = 2, cex = 0.7, adj = 0)
+    #----------------------------------------------------------
+    # Global Minimum risk portfolio 
+    #----------------------------------------------------------
+    globminSpec <- portfolioSpec()
+    globminPortfolio <- minvariancePortfolio(
+        data = myStocks2,
+        spec = globminSpec,
+        constraints = "LongOnly")
+    print(globminPortfolio)
+    
+    
+    
+    
+   col <- seqPalette(ncol(myStocks2), "YlGn")
+   weightsPie(globminPortfolio, box = FALSE, col = col)
+   mtext(text = "Global Minimum Variance MV Portfolio", side = 3,
+            line = 1.5, font = 2, cex = 0.7, adj = 0)
+   weightedReturnsPie(globminPortfolio, box = FALSE, col = col)
+   mtext(text = "Global Minimum Variance MV Portfolio", side = 3,
+            line = 1.5, font = 2, cex = 0.7, adj = 0)
+   covRiskBudgetsPie(globminPortfolio, box = FALSE, col = col)
+    mtext(text = "Global Minimum Variance MV Portfolio", side = 3,
+            line = 1.5, font = 2, cex = 0.7, adj = 0)
+    #----------------------------------------------------------
+    # Tangency portfolio 
+    #----------------------------------------------------------
+    tgSpec <- portfolioSpec()
+    setRiskFreeRate(tgSpec) <- 0.02
+    box.1<-paste("minW[1:",nStocks,"]=0.0",sep="");
+    box.2<-paste("maxW[1:",nStocks,"]=0.2",sep="");
+    boxConstraints<-c(box.1,box.2)
+    tgPortfolio <- tangencyPortfolio(
+        data = myStocks2,
+        spec = tgSpec,
+        constraints=boxConstraints)
+    print(tgPortfolio);    
+        
+    col <- seqPalette(ncol(myStocks2), "BuPu") 
+    weightsPie(tgPortfolio, box = FALSE, col = col)
+    mtext(text = "Tangency MV Portfolio", side = 3, line = 1.5,
+       font = 2, cex = 0.7, adj = 0)
+    weightedReturnsPie(tgPortfolio, box = FALSE, col = col)
+    mtext(text = "Tangency MV Portfolio", side = 3, line = 1.5,
+       font = 2, cex = 0.7, adj = 0)
+    covRiskBudgetsPie(tgPortfolio, box = FALSE, col = col)
+    mtext(text = "Tangency MV Portfolio", side = 3, line = 1.5,
+       font = 2, cex = 0.7, adj = 0) 
+
   
-  assetsCorEigenPlot(XLUData, method = "kendall")
-  #8.5
-  #---------grouping using kmeans analysis
- kmeans <- assetsSelect(XLUData, method = "kmeans", control <- c(centers = 10,
-                                                                    algorithm = "Hartigan-Wong"))
- sort(kmeans$cluster)
-     
-  #--------------------------------------------------------------------------------------------
-  # COMPARING MULTIVARIATE RETURN AND RISK STATISTICS
- assetsBasicStatsPlot(XLUData[, -8], title = "", description = "")
- assetsMomentsPlot(XLUData[, -8], title = "", description = "")    
- assetsBoxStatsPlot(XLUData[, -8], title = "", description = "")   
-   #---------------------------------------------------------------------------------------------
- #  PAIRWISE DEPENDENCIES OF ASSETS
- library(fMultivar)
- #Function:
- #     pairs displays pairs of scatterplots of assets
- # assetsPairsPlot displays pairs of scatterplots of assets
- # assetsCorgramPlot displays correlations between assets
- # assetsCorTestPlot displays and tests pairwise correlations
- # assetsCorImagePlot displays an image plot of correlations
- # squareBinning does a square binning of data points,
- # hexBinning does a hexagonal binning of data points
-     #SIMPLE PAIRWISE SCATTER PLOTS OF ASSETS
- #     We will rearrange the assets as suggested by hierarchical clustering. This
- # yields a nicer arrangement and view of the off-diagonal scatterplot panels
- # of the graph.
+    for (i in 1:3) plot(myStocks2[, (10 * i - 9):(10 * i)])
+    for (i in 1:3) plot(myStocks2[, (10 * i - 9):(10 * i)])
+    assetsCorImagePlot(myStocks2)
+    plot(assetsSelect(myStocks2))
+    frontier <- portfolioFrontier(myStocks2)
+    tailoredFrontierPlot(frontier)
+    weightsPlot(frontier)
+    #showClass("fPFOLIOBACKTEST")
+   
+    
+    #The backtester is hard to debug.  Try it manually
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    myBacktest <- portfolioBacktest()
+    setWindowsHorizon(myBacktest) <- "12m"
+    equidistWindows(data = myData2, backtest = myBacktest)   
+    
+  
+    mySpec <- tgSpec;#portfolioSpec()
+    myConstraints <- boxConstraints
+    myBacktest <- portfolioBacktest()
+    setWindowsHorizon(myBacktest) <- "18m"
+    setSmootherLambda(myBacktest) <- "3m";
+    myFormula<-paste(etfname,"=",paste(v.stocks,collapse="+"),sep="");
+    myFormula<-paste(etfname,"=",paste(v.stocks[1:9],collapse="+"),sep="");
+    myPortfolios <- portfolioBacktesting(formula = myFormula,
+                                            data = myData2, spec = mySpec, constraints = myConstraints,
+                                            backtest = myBacktest, trace = TRUE)
+    
+    }    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
  
- v.assets   <- assetsArrange(XLUData, method = "hclust");
- XLUData2<-100*XLUData[,v.assets];
- assetsPairsPlot(XLUData2,pch=19,cex=0.5,col="royalblue4");#Not useful for more than 10 variables
- v.sc<-seq(from=1,to=length(v.assets),by=4)
- assetsPairsPlot(XLUData2[,v.sc],pch=19,cex=0.5,col="royalblue4");
- assetsCorImagePlot(XLUData2[,v.sc])
- #----------------------------------------------------------------------------
- # Portfolio Framework
- #------------------------------------------------------------------------------
- library(fPortfolio);
- mySpec<-portfolioSpec();  #create default (MV) portfoliospec
- getWeights(mySpec)
- setWeights(mySpec) <- rep(1.0/28.0,28) 
- getTargetReturn(mySpec)
- getTargetRisk(mySpec)
- getOptimize(mySpec)
- setTargetReturn(mySpec) <- 0.025
- setTargetRisk(mySpec) <- 0.3
- setRiskFreeRate(mySpec)<-0.02
- 
- showClass("fPFOLIODATA")
- 
- myAssets<-100*XLUData[,v.assets[v.sc]];
- 
- myData<-portfolioData(data = myAssets, spec = portfolioSpec())
- str(myData)
- print(myData)
- getStatistics(myData)
- showClass("fPFOLIOCON")
- setTargetReturn(mySpec) <- mean(myAssets);
- Constraints <- "LongOnly"
- defaultConstraints <- portfolioConstraints(myData, mySpec, Constraints)
- str(defaultConstraints, width = 65, strict.width = "cut")
- print(defaultConstraints)
- showClass("fPORTFOLIO")
- tgPortfolio <- tangencyPortfolio( myAssets)
- str(tgPortfolio, width = 65, strict.width = "cut") 
- print(tgPortfolio);
- tgPortfolio@portfolio
- #mean-var portfolios
- 
- 
-    l.Res<-list();
-    return(l.Res)
-}
-
-> SPI.TP <- turnpoints(SPI.LW)
-> SPI.PEAKS <- SPI.TP[SPI.TP@recordIDs[, "peaks"] == TRUE, ]
-> SPI.PITS <- SPI.TP[SPI.TP@recordIDs[, "pits"] == TRUE, ]
-> plot(SPI)
-> lines(SPI.LW, col = "brown", lwd = 2)
-> points(SPI.PEAKS, col = "green3", pch = 24)
-> points(SPI.PITS, col = "red", pch = 25)
-
-
-
-
-> SPI <- SWX[, "SPI"]
-> SPI.LW <- lowess(SPI, f = 0.08)
-> plot(SPI)
-> lines(SPI.LW, col = "brown", lwd = 2)
-How
--- 1_GetSPDRs.R
+++ 1_GetSPDRs.R
@@ -43,15 +43,16 @@ registerDoSEQ()
 
 v.holdings    <- list.files(dirHoldings,include.dirs=FALSE);
 v.ETFs        <- str_sub(v.holdings,1,-7)
-df.ETFdict    <- df.EODTickers%>%dplyr::filter(Ticker %in% v.ETFs);
-ietf<-11;
+0
+<- df.EODTickers%>%dplyr::filter(Ticker %in% v.ETFs);
+ietf<-21;
 df.TargetDirectoryETFs$ETF<-as.character(df.TargetDirectoryETFs$ETF)
 df.dictentry<-df.TargetDirectoryETFs[ietf,]
 symb<-str_sub(df.dictentry$ETF,1,-3)
 fnin<-paste(dirPackages,"/",symb,"_P.xts",sep="")
 ss_df.x.xts<-load(fnin)
 names(df.x.xts)<-str_sub(names(df.x.xts),5,); #correct this in the 00_BuildHoldingsFiles.R file
-
+if(ietf==21){df.x.xts<-df.x.xts[,-10]}
 #ensure coloumns are in required order, where the target etf is the last column
 #  Should move to the the 00_BuildHoldingsFiles.R above----
 df.x<-f.xts2df(df.x.xts)
