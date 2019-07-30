311bee83a2891384e5ef5dbd19f1f83a6cf0f2e1
Dennis Warner <dennisleewarner@gmail.com>
2019-07-30 15:32:45 -0400

    First Commit,  Exhaustive Grid

---
+++ ADSEQ/main_ANDEQ.R
@@ -0,0 +1,60 @@
+#ANDEQ main routine
+#notation
+#Quantities
+#  L  factor 1,   K  factor 2,   A  good 1,   B good 2,  R  value of toal procution
+# Parameters
+#  p   price of A,   q  price of B
+#  omega   Share of L used in production of A
+#  theta   share of K used in  priduction of B
+#  alpha   exponent in A production function
+#  beta    exponent in B production function
+#
+#  The form of the production functions are given, and are embeded in two production functions
+#       f.A for the production of A and f.B for B
+f.A<-function(omega,L,theta,K,alpha){
+    A<-   ((omega*L*theta*K)/(omega*L+6))^alpha;
+    return(A)
+}
+f.B<-function(omega,L,theta,K,beta){
+    B<-(((1-omega)*L*(1-theta)*K)/((1-omega)*L+6))^beta;
+    return(B);
+}
+#set the constants and the initial parameter varlues
+L<-47.5829;
+K<-100;
+p<-1;
+q<-2;
+omega<-1;
+theta<-1;   # all production toward Good A
+alpha<- .7;
+beta <- .6;
+#   a Brute Force approach
+#   double loop over factor allocations,  build a 3d grid of revennue answers,  keep track of maximum
+stepsize<-.001;   # how much to vary the allocation shares
+v.omegaWeights<-seq(from=0 ,to =1, by = stepsize);
+v.thetaWeights<-v.omegaWeights;   #same grid for K share
+omega<-v.omegaWeights[2];
+theta<-v.thetaWeights[2];
+maxR<-0;
+for(omega in v.omegaWeights){
+    for(theta in v.thetaWeights){
+        
+        A<-f.A(omega,L,theta,K,alpha);
+        B<-f.B(omega,L,theta,K,beta);
+        R<-p*A+q*B;
+        #round answers for clean output
+        A<-round(A,digits=3);B=round(B,digits=3);R=round(R,digits=3);
+        if(R>maxR){
+            maxR<-R;
+            omegaMax<-omega;
+            thetaMax<-theta;
+            Amax<-A;
+            Bmax<-B;
+            cat(omega,theta,"A=",A,"B=",B,"R=",R,"Max so Far=",maxR,"omegaMax=",omegaMax,"thetaMax=",thetaMax,"Amax=",Amax,"Bmax-",Bmax,"\n");
+        }
+      
+        
+    }
+    
+}
+for
