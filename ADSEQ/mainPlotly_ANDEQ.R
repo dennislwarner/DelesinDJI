0c21fe1c3784f1f9d77082510fa5889448626430
Dennis Warner <dennisleewarner@gmail.com>
2019-07-31 23:17:31 -0400

    Completion of first pass.

--- ADSEQ/mainPlotly_ANDEQ.R
+++ ADSEQ/mainPlotly_ANDEQ.R
@@ -1,11 +1,11 @@
 #ANDEQ main routine
 #notation
 #Quantities
-#  L  factor 1,   K  factor 2,   A  good 1,   B good 2,  R  value of toal procution
+#  J  factor Information Kaqpital,   L  Labor,   A  good 1,   B good 2,  R  value of toal procution
 # Parameters
 #  p   price of A,   q  price of B
-#  omega   Share of L used in production of A
-#  theta   share of K used in  priduction of B
+#  omega   Share of J used in production of A
+#  theta   share of L used in  priduction of B
 #  alpha   exponent in A production function
 #  beta    exponent in B production function
 #
@@ -15,66 +15,90 @@
 # first load the library for 3d scatterplots;
 library(scatterplot3d);
 #try the plotly library
-library(plotly)
-f.A<-function(omega,L,theta,K,alpha){
-    A<-   ((omega*L*theta*K)/(omega*L+6))^alpha;
+suppressMessages(library(plotly));
+
+#define production functions
+
+f.A<-function(omega,J,theta,L,alpha){
+    A<-   ((omega*J*theta*L)/(omega*J+6))^alpha;
     return(A)
 }
-f.B<-function(omega,L,theta,K,beta){
-    B<-(((1-omega)*L*(1-theta)*K)/((1-omega)*L+6))^beta;
+f.B<-function(omega,J,theta,L,beta){
+    B<-(((1-omega)*J*(1-theta)*L)/((1-omega)*J+6))^beta;
     return(B);
 }
 
-
+#=============================================================================
 #set the constants and the initial parameter varlues
-L<-47.5829;
-K<-100;
+J<-47.5829;
+L<-100;
 p<-1;
 q<-2;
 omega<-1;
 theta<-1;   # all production toward Good A
 alpha<- .7;
 beta <- .6;
+#=============================================================================
+
 #   a Brute Force approach
 #   double loop over factor allocations,  build a 3d grid of revennue answers,  keep track of maximum
-stepsize<-.01;   # how much to vary the allocation shares
-v.omegaWeights<-seq(from=3*stepsize ,to =1, by = stepsize);
-v.thetaWeights<-v.omegaWeights;   #same grid for K share
+minparm        <- 0.2      # avoid the 
+maxparm        <- 0.8
+
+stepsize       <- 0.01;   # how much to vary the allocation shares
+v.omegaWeights <- seq(from=minparm ,to =maxparm, by = stepsize);
+v.thetaWeights <- v.omegaWeights;   #same grid for K share
 
 # define a matrix to hold the resulting coordinates for the two input parameters (omega and theta) and the resulting revenue R
 # there will be one row for each solution tried
 # the number of rows will be the product of the number of each input parameter
-Nrows<- length(v.omegaWeights)*length(v.thetaWeights);
+#also define an equivalent NxN matrix to enable a simple heat-map type numeric display in excel
+NN<-length(v.omegaWeights);
 # define a matrix with rows=Nrows and 3 columns 
-m.X <-matrix(0,Nrows,3); 
-colnames(m.X)<-c("omega","theta","R")
+m.X <-matrix(0,NN+1,NN+1); 
+
+Nrows<- length(v.omegaWeights)*length(v.thetaWeights);
+m.Z <- matrix(0,Nrows,3)
+colnames(m.Z)<-c("Info Cap","Labor","R")
 
 omega<-v.omegaWeights[2];
 theta<-v.thetaWeights[2];
 maxR<-0;
 count<-0;
+iJ<-0;
+iL<-0;
 for(omega in v.omegaWeights){
+    iJ<-iJ+1;
+    m.X[iJ+1,1]<-omega*J;
+    iL<-0;
     for(theta in v.thetaWeights){
-        
-        A<-f.A(omega,L,theta,K,alpha);
-        B<-f.B(omega,L,theta,K,beta);
+        iL<-iL+1;
+        m.X[1,iL+1]<-theta*L;
+        A<-f.A(omega,J,theta,L,alpha);
+        B<-f.B(omega,J,theta,L,beta);
         R<-p*A+q*B;
+        m.X[iJ+1,iL+1]<-R;
         #round answers for clean output
-        A<-round(A,digits=3);B=round(B,digits=3);R=round(R,digits=3);
+        A<-round(A,digits=3);
+        B=round(B,digits=3);
+        R=round(R,digits=3);
         count<-count+1;
-        m.X[count,]<-c(omega,theta,R)
+       m.Z[count,]<-c(omega*J,theta*L,R);
         if(R>maxR){
             maxR<-R;
             omegaMax<-omega;
             thetaMax<-theta;
             Amax<-A;
             Bmax<-B;
-            cat(omega,theta,"A=",A,"B=",B,"R=",R,"Max so Far=",maxR,"omegaMax=",omegaMax,"thetaMax=",thetaMax,"Amax=",Amax,"Bmax=",Bmax,"\n");
-        }
-      
-        
-    }
-    
-}
-scatterplot3d(m.X,highlight.3d = TRUE,col.axis="blue", col.grid="lightblue",
-              main="scatterplot3d - 2", pch=1)
+            cat(omega,theta,"A=",A,"B=",B,"R=",R,"Max so Far=",maxR,"omegaMax=",
+                omegaMax,"thetaMax=",thetaMax,"Amax=",Amax,"Bmax=",Bmax,"\n");
+        }}}
+
+#----Calculations are done,  store the output in graphic and table form
+
+scatterplot3d(m.Z,highlight.3d = TRUE,col.axis="blue", col.grid="lightblue",
+              main="Revenue Surface  Base Case ", pch=1);
+
+df.maxtable<-data.frame(m.X);
+fn<-"Maxtable_BaseCase.csv";
+write.csv(df.maxtable,file=fn);
